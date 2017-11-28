d55a288068703720e053243e2b21334ef2b51f89
BlackMagi018 <loganjer@mail.gvsu.edu>
2017-11-28 11:46:18 -0500

    updates

--- Game_Engine.py
+++ Game_Engine.py
@@ -20,7 +20,7 @@ class Game:
                 fighting = False
             elif text.lower() == "attack":
                 text = input("Choose an Weapons # to Use\n")
-                i = int(text)-1
+                i = int(text) - 1
                 candy = self.player.useWeapon(i)
                 self.player.location.damage(candy, self.player.attack)
                 self.checkPop()
@@ -68,9 +68,19 @@ class Game:
         if self.Map.totalMonsters == 0:
             self.GameOver = True
 
+    def intro(self):
+        print("Welecome to Zork\n")
+        print("It seemed like a normal Halloween Eve. You bought a lot of candy, ate a lot of candy" +
+              " and went to bed early. You had a lot trick-or-treating to do the next day.\n" +
+              "Unfortunately, when you woke up you discovered that the world was not how you left it."
+              "Batches of bad candy had transformed your friends and neighbors into all sorts\nof crazy monsters. "
+              "Somehow you missed the tainted candy; it is therefore up to you to save your neighborhood "
+              "and turn everyone back to normal.\n")
+
 
 if __name__ == '__main__':
     Game = Game()
+    Game.intro()
     Game.checkPop()
     while not Game.GameOver:
         Game.getPlayerStatus()
